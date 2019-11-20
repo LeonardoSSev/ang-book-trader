08f2d6c9f650831e6b41af944352f69db397065b
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-20 15:27:16 -0300

    Adds route for the register and the profile components. Also adds an wildcard route redirecting to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterComponent } from './modules/auth/register/register.component';
 import { LoginComponent } from './modules/auth/login/login.component';
+import { ProfileComponent } from './modules/user/profile/profile.component';
 
 
 const routes: Routes = [
   {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
 
