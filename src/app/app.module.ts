def4a58c7cd3babb4323e1b3cee3552458345ea5
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-20 14:31:38 -0300

    Installs materialize css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import 'materialize-css';
+//import { MaterializeModule } from 'angular2-materialize';
 import { LoginComponent } from './modules/auth/login/login.component';
+
 
 @NgModule({
   declarations: [
