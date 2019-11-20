15066a794e656e12a89f23fbd66e6a25a9108bb3
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-20 15:43:06 -0300

    Removes commented code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import 'materialize-css';
-//import { MaterializeModule } from 'angular2-materialize';
 import { LoginComponent } from './modules/auth/login/login.component';
 import { RegisterComponent } from './modules/auth/register/register.component';
 import { ProfileComponent } from './modules/user/profile/profile.component';
+
 
 
 @NgModule({
