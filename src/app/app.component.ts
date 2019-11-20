f961889bc685f1373c0ae95c84e20f4a2e0fa92a
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-20 19:25:39 -0300

    Listens for the emit of if the user is authenticated or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './modules/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'book-trader';
+export class AppComponent implements OnInit {
+
+  isUserAuthenticated$: Observable<boolean> | boolean = false;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.isUserAuthenticated$ = this.authService.userAuthenticatedEmitter;
+  }
+
+
 }
