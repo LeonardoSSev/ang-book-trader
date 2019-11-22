5408ac67b0feee09fb7d9579661e78fd1c25735c
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-22 16:46:58 -0300

    Implements the register component form and the doRegister() method

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  verifyIfFormIsValid() {
+    return !this.registerForm.valid
+  }
+
+  doRegister() {
+    let user = new User();
+
+    user.name = this.registerForm.value.name;
+    user.email = this.registerForm.value.email;
+    user.password = this.registerForm.value.password;
+
+    this.authService.doRegister(user);
   }
 
 }
