4eafa1ea0a2f51614f791e1909c632161beea2c9
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-22 21:13:39 -0300

    Adds the user service as a provider

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
-import { MyBooksComponent } from './my-books/my-books.component';
+import { MyBooksComponent } from './profile/my-books/my-books.component';
 import { UserRoutingModule } from './user-routing.module';
+import { UserService } from './user.service';
 
 
 
@@ -14,6 +15,9 @@ import { UserRoutingModule } from './user-routing.module';
   imports: [
     CommonModule,
     UserRoutingModule
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }
